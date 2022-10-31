bea026a1e9e86aa33bfacbbba171c7ce9eb44395
wogks <wogks27@naver.com>
2022-11-01 01:19:39 +0900

    Fix: 로그인 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:fire_prac_insta/root_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
